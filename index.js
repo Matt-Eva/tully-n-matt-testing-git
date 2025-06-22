505d0f2e8b21d600c107872a293f339a5a0d9e63
Matt Eva <matteweva@gmail.com>
2025-06-21 21:15:40 -0700

    added matthew's poop info

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function sayHi() {
   console.log("hi");
-  console.log("i pooped");
+  console.log(
+    "i pooped, and my name is matthew I poop pretty often, every mornign while drinking coffee, often twice a day, sometimes more if I need to or am anxious"
+  );
 }
